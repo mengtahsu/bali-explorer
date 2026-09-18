002431710929b6220d1eaf83aff5e05e6fae12e6
mengtahsu <mengtahsu@gmail.com>
2026-09-18 15:09:41 +0800

    Fix source loader import rewriting

--- docs/main.source.js
+++ docs/main.source.js
@@ -9,7 +9,7 @@ const parts = await Promise.all(
   )
 );
 let src = parts.join('');
-src = src.replace(/from\s+(['"] )\.\//g, (_, q) => 'from ' + q + base.href);
-src = src.replace(/import\s+(['"] )\.\//g, (_, q) => 'import ' + q + base.href);
+src = src.replace(/from\s+(['"])\.\//g, (_, q) => 'from ' + q + base.href);
+src = src.replace(/import\s+(['"])\.\//g, (_, q) => 'import ' + q + base.href);
 const url = URL.createObjectURL(new Blob([src], { type: 'text/javascript' }));
 await import(url);
